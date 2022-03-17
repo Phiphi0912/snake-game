9cb168fe3643aa60e66d9c29dd9fffb75c1ad650
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-03-17 22:04:55 +0800

    feat: add get login and register router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const user = require('./modules/user')
+
 router.get('/', (req, res) => res.render('index'))
+router.use('/users', user)
 
 module.exports = router
